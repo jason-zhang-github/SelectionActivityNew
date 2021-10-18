96d17655963816b03a1c8dc704904d44c1551803
jason-zhang-github <zhang.jason.2021@gmail.com>
2021-10-18 14:49:07 -0400

    Initialized fragment views

--- app/src/main/java/com/example/selectionactivity/MainActivity.kt
+++ app/src/main/java/com/example/selectionactivity/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import org.w3c.dom.Text
 import java.io.Serializable
 import android.content.Context
+import androidx.fragment.app.FragmentContainerView
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,38 +23,24 @@ class MainActivity : AppCompatActivity() {
         val myArray : Array<String> = resources.getStringArray(R.array.dunk_names)
         val dunks = generateTestData(myArray)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        // val imageView = findViewById<ImageView>(R.id.imageView)
-        // val textView = findViewById<TextView>(R.id.textView)
+        // initialize FragmentContainerViews
+        val listFragment = findViewById<FragmentContainerView>(R.id.fragmentContainerView)
+        val detailFragment = findViewById<FragmentContainerView>(R.id.fragmentContainerView2)
 
-        recyclerView.layoutManager = GridLayoutManager(this, 3)
+
 
         // View.OnClickListener is created in the activity
         // and then passed to adapter
         // This allows the onClick() callback
         // to have access to the activity's members
         val onClickListener = View.OnClickListener {
-            val itemPosition = recyclerView.getChildAdapterPosition(it)
             //imageView.setImageResource(dunks[itemPosition].resourceId)
             //textView.text = dunks[itemPosition].description
 
-            sendMessage(dunks[itemPosition])
-
         }
 
-        recyclerView.adapter = ImageAdapter(dunks, onClickListener)
+
     }
-
-
-    /*val stussy : String = getString(R.string.stussy)
-    val paris : String = getString(R.string.paris)
-    val mondrian : String = getString(R.string.mondrian)
-    val pigeon : String = getString(R.string.pigeon)
-    val strangelove : String = getString(R.string.strangelove)
-    val lobster : String = getString(R.string.lobster)
-    val viotech : String = getString(R.string.viotech)
-    val supreme : String = getString(R.string.supreme)
-    val heineken : String = getString(R.string.heineken)*/
 
     // Assign each dunk with proper description
     fun generateTestData(dunkdesc : Array<String>): Array<Dunk> {
@@ -66,19 +53,6 @@ class MainActivity : AppCompatActivity() {
             , Dunk(R.drawable.dunk_viotech, dunkdesc[6])
             , Dunk(R.drawable.dunk_supreme, dunkdesc[7])
             , Dunk(R.drawable.dunk_heineken, dunkdesc[8]))
-    }
-
-
-    fun sendMessage(dunk: Dunk)
-    {
-
-        // val viewText = findViewById<TextView>(R.id.textView) // Find textView
-        // val message = viewText.text.toString() // convert text to string
-        val intent = Intent(this, DisplayActivity::class.java) // Create intent
-        intent.putExtra("custom_image", dunk as Serializable) // Add Dunk to the intent
-        // intent.putExtra("descriptor", dunk.description)
-        startActivity(intent)
-
     }
 
 
