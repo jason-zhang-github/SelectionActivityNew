39610fcf2a68b43b28f7f0deafb6ea31f843056e
jason-zhang-github <zhang.jason.2021@gmail.com>
2021-10-19 23:57:18 -0400

    need to implement livedata

--- app/src/main/java/com/example/selectionactivity/DunkModel.kt
+++ app/src/main/java/com/example/selectionactivity/DunkModel.kt
@@ -1,8 +1,9 @@
 package com.example.selectionactivity
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 
-class DunkModel : ViewModel
+class DunkModel : ViewModel()
 {
-
+    private final MutableLiveData<User> userLiveData = new MutableLiveData<>()
 }
