6f24bc3a265795f879f82a49deda06f35454e707
jason-zhang-github <zhang.jason.2021@gmail.com>
2021-10-20 16:57:22 -0400

    Finished DunkModel

--- app/src/main/java/com/example/selectionactivity/DunkModel.kt
+++ app/src/main/java/com/example/selectionactivity/DunkModel.kt
@@ -2,8 +2,24 @@ package com.example.selectionactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 
 class DunkModel : ViewModel()
 {
-    private final MutableLiveData<User> userLiveData = new MutableLiveData<>()
+    val dunk = MutableLiveData<Dunk>()
+
+    fun getDunk() : LiveData<Dunk>
+    {
+        return dunk
+    }
+
+    fun setDunk(_dunk : Dunk)
+    {
+        dunk.value = _dunk
+    }
+
+
+
 }
